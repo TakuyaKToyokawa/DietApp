b9b5a1838634c4b761dfe2f9bc6dfc64042dd8e7
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2021-02-12 16:19:42 -0800

    fix for cors

--- server.js
+++ server.js
@@ -4,7 +4,22 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
+app.use('/', function (req, res, next) {
+  //var allowedOrigins = ['http://localhost:3000'];
+  var origin = req.headers.origin;
+  // if(allowedOrigins.indexOf(origin) > -1){
+  //      res.setHeader('Access-Control-Allow-Origin', origin);
+  // }
+  res.setHeader('Access-Control-Allow-Origin', origin || "*");
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 //Food API
 app.get("/api/foods", (req, res) => {
